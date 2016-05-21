fd43550709bba361b296d54324868fb3ac61693e
Luis Matheus Paris <ing.luis.matheus@outlook.com>
2016-05-21 16:41:00 -0500

    updated event donation methods

--- server/methods/event-donations-methods.js
+++ server/methods/event-donations-methods.js
@@ -2,6 +2,17 @@
 Meteor.methods({
   'event_donations.create'(data){
     check(data, Object);
+    'event_donations.create'(data){
+    const user = Meteor.users.findOne({_id: this.userId});
+
+    if(user.profile.type == "volunteer"){
+      data.type = "volunteer";
+    }else{
+      let sponsorId = user.profile.sponsorId;
+      data.sponsorId = sponsorId;
+      data.type = "sponsor";
+    }
+
     EventDonations.insert(data);
   },
   'event_donations.update'(objectId, data){
