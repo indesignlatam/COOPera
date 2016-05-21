2b1cd4270d04b921ba6ebb4af484594d91f3d17d
Luis Matheus Paris <ing.luis.matheus@outlook.com>
2016-05-21 18:32:27 -0500

    created myEvents helper

--- collections/users.js
+++ collections/users.js
@@ -10,10 +10,44 @@ Users.helpers({
 
         let query = {};
         if (user.profile.categories && user.profile.categories.length > 0) {
-            query.categoryId = {
+            query.category = {
                 $in: user.profile.categories
             };
         }
+
         return Events.find(query);
+    },
+    myEvents(){
+      let user = Meteor.user()
+      let today = new Date();
+      if(Helpers.isVolunteer()){
+  			let volunteers = Volunteers.find({ volunteerId: Meteor.userId() }, {fields: { eventId: 1 }});
+  			let eventIDs = [];
+
+  			volunteers.map((volunteer)=>{
+  				eventIDs.push(volunteer.eventId);
+  			})
+
+  			let cursor = Events.find({_id: {$in: eventIDs}, scheduledDate:{	$gte: today }}, { sort: { scheduledDate:1 }}, { fields: Events.basicFields});
+        return cursor;
+  		}
+  		else if(Helpers.isOrganization()){
+  			let organizationId = user.profile.organizationId;
+  			let cursor = Events.find({organizationId:organizationId, scheduledDate:{	$gte: today }}, { fields: Events.basicFields});
+  			return cursor;
+  		}
+  		else if(Helpers.isSponsor()){
+  			let sponsorId = user.profile.sponsorId;
+  			let donations = EventDonations.find({sponsorId:sponsorId}, {fields:{ eventId: 1 }});
+  			let eventIds = [];
+
+  			donations.map((donation)=>{
+  				eventIds.push(donation.eventId)
+  			});
+
+  			let uniqueEventIds = _.uniq(eventIds);
+  			let cursor =  Events.find({_id: {$in: uniqueEventIds}, scheduledDate:{ $gte: today }},{fields: Events.basicFields });
+  			return cursor;
+  		}
     }
 });
