1927e8bf43d12427c4e5bd8ab1ed85265b8e34ce
Paulo Mogollon <wanchopeblanco@gmail.com>
2016-05-21 12:17:14 -0500

    Added multiple models

---
+++ collections/event-donations.js
@@ -0,0 +1,79 @@
+EventDonations = new Mongo.Collection('event-donations');
+
+
+/*
+ * Collection schema
+ *
+ */
+const EventDonationSchema = new SimpleSchema({
+	ammount: {
+		type: Number,
+		label: "Monto donado"
+	},
+	eventId: {
+		type: String,
+		label: "ID del evento"
+	},
+	sponsorId: {
+		type: String,
+		label: "ID del sponsor"
+	},
+	createdAt: {
+		type: Date,
+		autoValue() {
+			if(this.isInsert){
+				return new Date();
+			}else{
+				this.unset();
+			}
+		}
+	},
+	author: {
+		type: String,
+		autoValue() {
+			if (this.isInsert) {
+				return this.userId;
+			}
+		}
+	}
+});
+
+EventDonations.attachSchema(EventDonationSchema);
+
+/*
+ * Collection fields
+ * Public fields
+ *
+ */
+EventDonations.publicFields = {
+	name: true,
+}
+
+
+/*
+ * Collection helpers
+ *
+ */
+EventDonations.helpers({
+	// helper() {
+	// 	return '';
+	// },
+});
+
+
+/*
+ * Collection permissions
+ * Deny all actions on client-side
+ *
+ */
+EventDonations.deny({
+	insert() {
+		return true;
+	},
+	update() {
+		return true;
+	},
+	remove() {
+		return true;
+	}
+});
