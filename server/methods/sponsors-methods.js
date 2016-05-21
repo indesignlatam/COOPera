ac59da076647394ac240d8370be1004871fb8fae
Luis Matheus Paris <ing.luis.matheus@outlook.com>
2016-05-21 16:01:53 -0500

    updated sponsors methods

--- server/methods/sponsors-methods.js
+++ server/methods/sponsors-methods.js
@@ -2,15 +2,15 @@
 Meteor.methods({
   'sponsors.create'(data){
     check(data, Object);
-    Sponsors.insert(data);
+    return Sponsors.insert(data);
   },
   'sponsors.update'(objectId, data){
     check(data, Object);
     check(objectId, String);
-    Sponsors.update({_id: objectId}, {$set: data});
+    return Sponsors.update({_id: objectId}, {$set: data});
   },
   'sponsors.delete'(objectId){
     check(objectId, String);
-    Sponsors.remove(objectId);
+    return Sponsors.remove(objectId);
   }
 });
