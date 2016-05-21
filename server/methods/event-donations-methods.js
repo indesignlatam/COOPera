ef565b7f22f11bb4b574f24e73f96e5fcc72f33e
Luis Matheus Paris <ing.luis.matheus@outlook.com>
2016-05-21 12:49:49 -0500

    udpated event donations methods

--- server/methods/event-donations-methods.js
+++ server/methods/event-donations-methods.js
@@ -1,15 +1,15 @@
 // Event Donations Methods
 Meteor.methods({
-  'volunteers.create'(data){
+  'event_donations.create'(data){
     check(data, Object);
     EventDonations.insert(data);
   },
-  'volunteers.update'(objectId, data){
+  'event_donations.update'(objectId, data){
     check(data, Object);
     check(objectId, String);
     EventDonations.update({_id: objectId}, {$set: data});
   },
-  'volunteers.delete'(objectId){
+  'event_donations.delete'(objectId){
     check(objectId, String);
     EventDonations.remove(objectId);
   }
