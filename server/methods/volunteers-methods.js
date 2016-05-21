737fa2c3cfb067fd03843e4a0d3edfcf2ceef3c6
Luis Matheus Paris <ing.luis.matheus@outlook.com>
2016-05-21 16:18:09 -0500

    updated volunteers methods

--- server/methods/volunteers-methods.js
+++ server/methods/volunteers-methods.js
@@ -1,16 +1,13 @@
 // Volunteers Methods
 Meteor.methods({
-  'volunteers.create'(data){
-    check(data, Object);
-    Volunteers.insert(data);
+  'volunteers.create'(eventId){
+    check(eventId, String);
+    Volunteers.insert({ eventId: eventId, volunteerId: Meteor.userId() });
   },
   'volunteers.update'(objectId, data){
-    check(data, Object);
-    check(objectId, String);
     Volunteers.update({_id: objectId}, {$set: data});
   },
-  'volunteers.delete'(objectId){
-    check(objectId, String);
-    Volunteers.remove(objectId);
+  'volunteers.delete'(eventId){
+    Volunteers.remove({ eventId: eventId, volunteerId: Meteor.userId() });
   }
 });
